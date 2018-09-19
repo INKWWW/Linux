969a7d2b419df12efe885f8090d49aa28f127752
INKWWW <whmink@foxmail.com>
2018-09-19 16:26:33 +0800

    modify

--- shell/query_hql_inShell.sh
+++ shell/query_hql_inShell.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hive -f /home/hanmo.wang/check2.sql  --hivevar source_table=$1 --hivevar row_num=$2
+hive -f /home/XXX/query_hql_inShell.sql  --hivevar source_table=$1 --hivevar row_num=$2
